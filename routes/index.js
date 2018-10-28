eedc19e92d743c7b13270769c6822a4c55e755c3
Francisco Mercado <sissyyayle0517@hotmail.com>
2018-10-27 20:41:08 -0500

    making forms and packs services

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,11 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Disfruta de la verdadera experiencia en Cuba, solo con nosotros' });
 });
 
+router.get('/reservation', (req,res) => {
+  res.render('reservation')
+})
+router.get('/packs', (req,res) => {
+  res.render('packs')
+})
+
 module.exports = router;
